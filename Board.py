2b9e525594bab7d4b5b2fe05a0f2b3cd7d447566
xa31er <139361667+xa31er@users.noreply.github.com>
2023-09-17 12:06:29 +0200

    v0.1.1
    fixed issue #1 and added sound

--- Board.py
+++ Board.py
@@ -5,14 +5,14 @@ class board(object):
     def __init__(self):
         self.tmp = None
         self.board = [
-            ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
-            ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
-            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-            ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
-            ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']
+            ["r", "n", "b", "q", "k", "b", "n", "r"],
+            ["p", "p", "p", "p", "p", "p", "p", "p"],
+            [" ", " ", " ", " ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " ", " "],
+            ["P", "P", "P", "P", "P", "P", "P", "P"],
+            ["R", "N", "B", "Q", "K", "B", "N", "R"]
         ]
         self.WIDTH = self.HEIGHT = 512
         self.SIZE = (self.WIDTH, self.HEIGHT)
@@ -39,9 +39,7 @@ class board(object):
         pygame.display.set_icon(self.W_KNIGHT_IMAGE)
 
     def set(self, posx, posy, piece):
-        tmp = self.board[posx]
-        tmp[posy] = piece
-        self.board[posx] = tmp
+        self.board[posy][posx] = piece
 
     def draw_board(self):
         for row in range(8):
@@ -57,29 +55,29 @@ class board(object):
         for row in range(8):
             for col in range(8):
                 piece = self.board[row][col]
-                if piece == 'P':
+                if piece == "P":
                     self.screen.blit(self.W_PAWN_IMAGE, (col * self.SQUARE_SIZE, row * self.SQUARE_SIZE))
-                elif piece == 'R':
+                elif piece == "R":
                     self.screen.blit(self.W_ROOK_IMAGE, (col * self.SQUARE_SIZE, row * self.SQUARE_SIZE))
-                elif piece == 'N':
+                elif piece == "N":
                     self.screen.blit(self.W_KNIGHT_IMAGE, (col * self.SQUARE_SIZE, row * self.SQUARE_SIZE))
-                elif piece == 'B':
+                elif piece == "B":
                     self.screen.blit(self.W_BISHOP_IMAGE, (col * self.SQUARE_SIZE, row * self.SQUARE_SIZE))
-                elif piece == 'Q':
+                elif piece == "Q":
                     self.screen.blit(self.W_QUEEN_IMAGE, (col * self.SQUARE_SIZE, row * self.SQUARE_SIZE))
-                elif piece == 'K':
+                elif piece == "K":
                     self.screen.blit(self.W_KING_IMAGE, (col * self.SQUARE_SIZE, row * self.SQUARE_SIZE))
-                elif piece == 'p':
+                elif piece == "p":
                     self.screen.blit(self.B_PAWN_IMAGE, (col * self.SQUARE_SIZE, row * self.SQUARE_SIZE))
-                elif piece == 'r':
+                elif piece == "r":
                     self.screen.blit(self.B_ROOK_IMAGE, (col * self.SQUARE_SIZE, row * self.SQUARE_SIZE))
-                elif piece == 'n':
+                elif piece == "n":
                     self.screen.blit(self.B_KNIGHT_IMAGE, (col * self.SQUARE_SIZE, row * self.SQUARE_SIZE))
-                elif piece == 'b':
+                elif piece == "b":
                     self.screen.blit(self.B_BISHOP_IMAGE, (col * self.SQUARE_SIZE, row * self.SQUARE_SIZE))
-                elif piece == 'q':
+                elif piece == "q":
                     self.screen.blit(self.B_QUEEN_IMAGE, (col * self.SQUARE_SIZE, row * self.SQUARE_SIZE))
-                elif piece == 'k':
+                elif piece == "k":
                     self.screen.blit(self.B_KING_IMAGE, (col * self.SQUARE_SIZE, row * self.SQUARE_SIZE))
 
     @staticmethod
@@ -92,42 +90,59 @@ class board(object):
                          (self.calc_mouse()[0] * 64, self.calc_mouse()[1] * 64, self.SQUARE_SIZE, self.SQUARE_SIZE), )
 
     def draw_mouse_piece(self, piece):
-        if piece == 'P':
+        if piece == "P":
             self.screen.blit(self.W_PAWN_IMAGE, ([pygame.mouse.get_pos()[0] - 32, pygame.mouse.get_pos()[1] - 32]))
-        elif piece == 'R':
+        elif piece == "R":
             self.screen.blit(self.W_ROOK_IMAGE, ([pygame.mouse.get_pos()[0] - 32, pygame.mouse.get_pos()[1] - 32]))
-        elif piece == 'N':
+        elif piece == "N":
             self.screen.blit(self.W_KNIGHT_IMAGE, ([pygame.mouse.get_pos()[0] - 32, pygame.mouse.get_pos()[1] - 32]))
-        elif piece == 'B':
+        elif piece == "B":
             self.screen.blit(self.W_BISHOP_IMAGE, ([pygame.mouse.get_pos()[0] - 32, pygame.mouse.get_pos()[1] - 32]))
-        elif piece == 'Q':
+        elif piece == "Q":
             self.screen.blit(self.W_QUEEN_IMAGE, ([pygame.mouse.get_pos()[0] - 32, pygame.mouse.get_pos()[1] - 32]))
-        elif piece == 'K':
+        elif piece == "K":
             self.screen.blit(self.W_KING_IMAGE, ([pygame.mouse.get_pos()[0] - 32, pygame.mouse.get_pos()[1] - 32]))
-        elif piece == 'p':
+        elif piece == "p":
             self.screen.blit(self.B_PAWN_IMAGE, ([pygame.mouse.get_pos()[0] - 32, pygame.mouse.get_pos()[1] - 32]))
-        elif piece == 'r':
+        elif piece == "r":
             self.screen.blit(self.B_ROOK_IMAGE, ([pygame.mouse.get_pos()[0] - 32, pygame.mouse.get_pos()[1] - 32]))
-        elif piece == 'n':
+        elif piece == "n":
             self.screen.blit(self.B_KNIGHT_IMAGE, ([pygame.mouse.get_pos()[0] - 32, pygame.mouse.get_pos()[1] - 32]))
-        elif piece == 'b':
+        elif piece == "b":
             self.screen.blit(self.B_BISHOP_IMAGE, ([pygame.mouse.get_pos()[0] - 32, pygame.mouse.get_pos()[1] - 32]))
-        elif piece == 'q':
+        elif piece == "q":
             self.screen.blit(self.B_QUEEN_IMAGE, ([pygame.mouse.get_pos()[0] - 32, pygame.mouse.get_pos()[1] - 32]))
-        elif piece == 'k':
+        elif piece == "k":
             self.screen.blit(self.B_KING_IMAGE, ([pygame.mouse.get_pos()[0] - 32, pygame.mouse.get_pos()[1] - 32]))
 
-    def drag_n_drop(self):
-        mousex = self.calc_mouse()[0]
-        mousey = self.calc_mouse()[1]
-        while pygame.mouse.get_pressed()[0]:
-            self.draw_board()
-            self.draw_pieces()
-            self.draw_mouse_piece(self.board[mousex][mousey])
-            pygame.display.flip()
-        else:
-            self.set(self.calc_mouse()[0], self.calc_mouse()[1], self.board[mousex][mousey])
+    def tick(self):
+        self.draw_board()
+        self.highlight()
+        self.draw_pieces()
+        pygame.display.flip()
+        if pygame.event.get(pygame.MOUSEBUTTONDOWN):
+            mousex = self.calc_mouse()[0]
+            mousey = self.calc_mouse()[1]
+            piece = self.board[mousey][mousex]
             self.set(mousex, mousey, " ")
+            holding = True
+            while holding:
+                self.draw_board()
+                self.highlight()
+                self.draw_pieces()
+                self.draw_mouse_piece(piece)
+                pygame.display.flip()
+                if pygame.event.get(pygame.MOUSEBUTTONUP):
+                    mousex = self.calc_mouse()[0]
+                    mousey = self.calc_mouse()[1]
+                    if self.board[mousey][mousex] == " ":
+                        pygame.mixer.Sound.play(self.move_sound)
+                        print("empty")
+                    else:
+                        pygame.mixer.Sound.play(self.capture_sound)
+                        print("full")
+                    self.set(mousex, mousey, piece)
+                    holding = False
 
     """def calc_pieces(self):
         print("TODO")
--- Chess.py
+++ Chess.py
@@ -1,21 +1,19 @@
 from Board import *
-
+import pygame
 pygame.init()
 
 chess = board()
 
 
 def update_board():
-    chess.drag_n_drop()
-    pygame.display.flip()
+    chess.tick()
     pass
 
 
 running = True
 while running:
     update_board()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    if pygame.event.get(pygame.QUIT):
+        running = False
     pass
 pygame.quit()
