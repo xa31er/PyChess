334fb977db74ad9cbc59a03f80112b264d152b02
xa31er <condexabier@gmail.com>
2024-10-28 13:31:27 +0100

    - Update .gitignore - Refactor Piece class - Cleaning Board class

--- Pychess/Piece.py
+++ Pychess/Piece.py
@@ -1,8 +1,23 @@
 import pygame as pg
 
+class PieceTypeNotValidError(Exception):
+    """Raised when trying to assign an invalid piece type to a piece"""
+    def __init__(self, piece_type):
+        self.value = piece_type
+    def __str__(self):
+        return(repr(self.value))
 
 class Piece(object):
-    def __init__(self, screen, square_size, piece_type):
+    def __init__(self, piece_type : str = "No type Specified!"):
+        # Pygame Init
+        pg.init()
+
+        # Valid Pieces
+        self.pieces = [
+            "P", "p", "r", "N", "n", "B", "b", "K", "k", "Q", "q",
+        ]
+
+        # Piece Sprites
         self.images = {
             "P": pg.image.load('Assets/wp.png'),
             "p": pg.image.load('Assets/bp.png'),
@@ -17,14 +32,26 @@ class Piece(object):
             "Q": pg.image.load('Assets/wq.png'),
             "q": pg.image.load('Assets/bq.png')
         }
+
+        # Sounds
         self.move_sound = pg.mixer.Sound("Assets/move-self.mp3")
         self.capture_sound = pg.mixer.Sound("Assets/capture.mp3")
-        self.screen = screen
-        self.type = piece_type
+        
+        # Piece variables
+        try:
+            assert self.is_valid_type(piece_type)
+        except AssertionError:
+            raise PieceTypeNotValidError(piece_type)
+
         self.row = 0
         self.col = 0
-        self.isWhite = True
-        self.square_size = square_size
+        
+        # Set piece color 
+        if piece_type[0].upper() == piece_type:
+            self.isWhite = True
+        else:
+            self.isWhite = False
+        
 
     def row_col_to_index(self):
         index = ((self.row - 1) * 8) + self.col
@@ -33,12 +60,21 @@ class Piece(object):
     def draw_piece(self):
         self.screen.blit(self.images[self.type], (self.col * self.square_size, self.row * self.square_size))
 
-    def setType(self, new_type):
+    def set_type(self, new_type):
         self.type = new_type
         if new_type[0].upper() == new_type:
             self.isWhite = True
         else:
             self.isWhite = False
 
-    def getType(self):
+    def get_type(self):
         return self.type[0]
+    
+    def is_valid_type(self, type_to_check : str):
+        if type_to_check in self.pieces:
+            return True
+        else:
+            return False
+
+if __name__ == "__main__":
+    test_piece = Piece("p")
